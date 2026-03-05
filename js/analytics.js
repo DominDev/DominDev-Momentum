f9d6b6041e025eb4cfb4dec1096705c0b05daf8b
DominDev <pawel.dominiak@domindev.com>
2026-03-05 14:11:52 +0100

    GA4 csp

---
+++ js/analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics 4 - G-V80QM5EV03
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-V80QM5EV03');
